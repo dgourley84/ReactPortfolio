bec7922679d151dcd2d8b15d9798915cff7fea52
dgourley84 <81537606+dgourley84@users.noreply.github.com>
2023-01-05 17:09:37 +1000

    NavBar section of protfolio with responsive behavior

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import Navbar from "./components/Navbar"
+import { useEffect, useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
+import DotGroup from "./components/DotGroup.jsx";
 
 function App (){
+    const [selectedPage, setSelectedPage] = useState("home");
+    const [isTopOfPage, setIsTopOfPage] = useState(true);
+    const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
+    useEffect (()=>{
+        const handleScroll = ()=>{
+            if (window.scrollY ===0) setIsTopOfPage(true); //if page is scrolled away from top of window set navbar colour to red
+            if (window.scrollY !==0) setIsTopOfPage(false); //if page hasnt scrolled then keep orginal background colour
+        }
+        window.addEventListener("scroll",handleScroll); //listen for scroll on page
+        return () => window.removeEventListener("scroll",handleScroll);
+    },[]);
+
     return (
-        <div className='App'>
+        <div className="app bg-deep-blue">
+            <Navbar 
+            isTopOfPage ={isTopOfPage} 
+            selectedPage ={selectedPage} 
+            setSelectedPage={setSelectedPage}
+            />
+            <div className="w-5/6 mx-auto md:h-full">
+                {isAboveMediumScreens && (
+                    <DotGroup
+                        selectedPage={selectedPage}
+                        setSelectedPage={setSelectedPage}
+                    />
+                )}
+            </div>
         </div>
     );
 }
