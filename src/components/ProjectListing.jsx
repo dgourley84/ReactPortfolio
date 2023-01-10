ffb40ade5a2795f53efc66ff932cd132173366b9
dgourley84 <81537606+dgourley84@users.noreply.github.com>
2023-01-10 17:02:05 +1000

    Addition of all projects as well as function to map

--- src/components/ProjectListing.jsx
+++ src/components/ProjectListing.jsx
@@ -1,5 +1,4 @@
 import { motion } from "framer-motion";
-import TextEditorProjectImage from "../assets/TextEditorProject.png"
 
 const projectVariant = {
     hidden: { opacity: 0, scale: 0.8 },
@@ -9,34 +8,20 @@ const projectVariant = {
 const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
   bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
 
-const ProjectOne = ({ title }) => {
 
-  return (
-    <motion.div variants={projectVariant} className="relative">
-      <div className={overlayStyles}>
-        <p className="text-2xl font-playfair">Text Editor Project</p>
-        <p className="mt-7">
-            This app is an online text editor that allows programmers to write code snippets in the browser and store them for later.
-        </p>
-      </div>
-      <img src={TextEditorProjectImage} alt="TextEditorProject" />
-    </motion.div>
-  );
-};
-
-const ProjectTwo = ({ title }) => {
+const Project = (props) => {
   
   return (
     <motion.div variants={projectVariant} className="relative">
       <div className={overlayStyles}>
-        <p className="text-2xl font-playfair">Text Editor Project</p>
+        <p className="text-2xl font-playfair">{props.title}</p>
         <p className="mt-7">
-            This app is an online text editor that allows programmers to write code snippets in the browser and store them for later.
+            {props.description}
         </p>
       </div>
-      <img src={TextEditorProjectImage} alt="TextEditorProject" />
+      <img src={props.img} alt={props.alt} />
     </motion.div>
   );
-};
+}
 
-export {ProjectOne, ProjectTwo};
+export default Project;
